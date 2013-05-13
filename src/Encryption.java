ffd9c9994af0a449b93bde2877d59b084959b440
mjeries <mjeries@gmail.com>
2013-05-13 16:48:36 -0500

    encryption
    added one way (server-client) encryption. also added disconnect
    notification to server console. removed junk entry from classpath.

--- src/Encryption.java
+++ src/Encryption.java
@@ -10,139 +10,134 @@ import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
-public class Encryption
-{
+public class Encryption {
 
+	/*
+	
+	public static void main(String[] args) {
+		Security.addProvider(new BouncyCastleProvider());
 
+		String plainText = "This is the chat message";
+		System.out.println("plaintext: " + plainText);
 
-	public static void main(String[] args)
-	{
-			Security.addProvider(new BouncyCastleProvider());
+		String key = "000102030405060708090A0B0C0D0E0F";
+		System.out.println("key: " + key);
 
-		    String plainText = "This is the chat message";
-		    System.out.println("plaintext: " + plainText);
+		String cipherText = encrypt(plainText, key);
+		System.out.println("ciphertext: " + cipherText);
 
-		    String key = "000102030405060708090A0B0C0D0E0F";
-		    System.out.println("key: " + key);
+		String decrypted = decrypt(cipherText, key);
 
-		    String cipherText = encrypt(plainText, key);
-		    System.out.println("ciphertext: " + cipherText);
+		if (decrypted != null && decrypted.equals(plainText)) {
+			System.out.println("Decryption: " + decrypted);
+		} else {
+			System.out.println("Decryption Failed!");
+		}
 
-		    String decrypted = decrypt(cipherText, key);
+	}*/
 
+	public static String encrypt(final String plainText, final String symKeyHex) {
 
+		final byte[] symKeyData = DatatypeConverter.parseHexBinary(symKeyHex);
+		final byte[] encodedMessage = plainText.getBytes(Charset
+				.forName("UTF-8"));
 
-		    if (decrypted != null && decrypted.equals(plainText)) {
-		        System.out.println("Decryption: " + decrypted);
-		    } else {
-		        System.out.println("Decryption Failed!");
-    }
+		try {
+
+			// create new cipher using AES 128 CBC
+
+			final Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding", "BC");
+			final int blockSize = cipher.getBlockSize();
+
+			// generate the key
+
+			final SecretKeySpec symKey = new SecretKeySpec(symKeyData, "AES");
+
+			// generate the iv
+
+			final byte[] ivData = new byte[blockSize];
+			final SecureRandom rnd = SecureRandom.getInstance("SHA1PRNG");
+			rnd.nextBytes(ivData);
+			final IvParameterSpec iv = new IvParameterSpec(ivData);
+
+			// encrypt the cipher
+
+			cipher.init(Cipher.ENCRYPT_MODE, symKey, iv);
+			final byte[] cipherText = cipher.doFinal(encodedMessage);
+
+			// concatenate IV and encrypted message
+
+			final byte[] ivCipherText = new byte[ivData.length
+					+ cipherText.length];
+			System.arraycopy(ivData, 0, ivCipherText, 0, blockSize);
+			System.arraycopy(cipherText, 0, ivCipherText, blockSize,
+					cipherText.length);
+			final String ivCipherTextBase64 = DatatypeConverter
+					.printBase64Binary(ivCipherText);
+
+			// return the cipher text in Base 64
+
+			return ivCipherTextBase64;
+
+		}// try
+
+		catch (InvalidKeyException e) {
+			throw new IllegalArgumentException(
+					"key argument does not contain a valid AES key");
+		} catch (GeneralSecurityException e) {
+			throw new IllegalStateException(
+					"Unexpected exception during encryption", e);
+		}
 
 	}
 
+	public static String decrypt(final String ivCipherTextBase64,
+			final String symKeyHex) {
 
-public static String encrypt(final String plainText, final String symKeyHex) {
+		final byte[] symKeyData = DatatypeConverter.parseHexBinary(symKeyHex);
+		final byte[] ivCipherText = DatatypeConverter
+				.parseBase64Binary(ivCipherTextBase64);
 
-        final byte[] symKeyData = DatatypeConverter.parseHexBinary(symKeyHex);
-        final byte[] encodedMessage = plainText.getBytes(Charset.forName("UTF-8"));
+		try {
 
-        try {
+			final Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding", "BC");
+			final int blockSize = cipher.getBlockSize();
 
-        	//create new cipher using AES 128 CBC
+			// create the key
+			final SecretKeySpec symKey = new SecretKeySpec(symKeyData, "AES");
 
-            final Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding", "BC");
-            final int blockSize = cipher.getBlockSize();
+			// retrieve random IV from start of the received message
 
-            // generate the key
+			final byte[] ivData = new byte[blockSize];
+			System.arraycopy(ivCipherText, 0, ivData, 0, blockSize);
+			final IvParameterSpec iv = new IvParameterSpec(ivData);
 
-            final SecretKeySpec symKey = new SecretKeySpec(symKeyData, "AES");
+			// retrieve the cipher text
+			final byte[] cipherText = new byte[ivCipherText.length - blockSize];
+			System.arraycopy(ivCipherText, blockSize, cipherText, 0,
+					cipherText.length);
 
-            // generate the iv
+			cipher.init(Cipher.DECRYPT_MODE, symKey, iv);
 
-            final byte[] ivData = new byte[blockSize];
-            final SecureRandom rnd = SecureRandom.getInstance("SHA1PRNG");
-            rnd.nextBytes(ivData);
-            final IvParameterSpec iv = new IvParameterSpec(ivData);
+			final byte[] encodedMessage = cipher.doFinal(cipherText);
 
+			// concatenate IV and encrypted message
+			final String plainText = new String(encodedMessage,
+					Charset.forName("UTF-8"));
 
+			return plainText;
 
-            // encrypt the cipher
-
-            cipher.init(Cipher.ENCRYPT_MODE, symKey, iv);
-            final byte[] cipherText = cipher.doFinal(encodedMessage);
-
-            // concatenate IV and encrypted message
-
-            final byte[] ivCipherText = new byte[ivData.length + cipherText.length];
-            System.arraycopy(ivData, 0, ivCipherText, 0, blockSize);
-            System.arraycopy(cipherText, 0, ivCipherText, blockSize, cipherText.length);
-            final String ivCipherTextBase64 = DatatypeConverter.printBase64Binary(ivCipherText);
-
-            //return the cipher text in Base 64
-
-            return ivCipherTextBase64;
-
-
-        }//try
-
-        catch (InvalidKeyException e) {
-            throw new IllegalArgumentException(
-                    "key argument does not contain a valid AES key");
-        } catch (GeneralSecurityException e) {
-            throw new IllegalStateException(
-                    "Unexpected exception during encryption", e);
-        }
-
-
-    }
-
-    public static String decrypt(final String ivCipherTextBase64, final String symKeyHex) {
-
-        final byte[] symKeyData = DatatypeConverter.parseHexBinary(symKeyHex);
-        final byte[] ivCipherText = DatatypeConverter.parseBase64Binary(ivCipherTextBase64);
-
-
-        try {
-
-            final Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding","BC");
-            final int blockSize = cipher.getBlockSize();
-
-            // create the key
-            final SecretKeySpec symKey = new SecretKeySpec(symKeyData, "AES");
-
-            // retrieve random IV from start of the received message
-
-            final byte[] ivData = new byte[blockSize];
-            System.arraycopy(ivCipherText, 0, ivData, 0, blockSize);
-            final IvParameterSpec iv = new IvParameterSpec(ivData);
-
-            // retrieve the cipher text
-            final byte[] cipherText = new byte[ivCipherText.length - blockSize];
-            System.arraycopy(ivCipherText, blockSize, cipherText, 0, cipherText.length);
-
-            cipher.init(Cipher.DECRYPT_MODE, symKey, iv);
-
-            final byte[] encodedMessage = cipher.doFinal(cipherText);
-
-            // concatenate IV and encrypted message
-            final String plainText = new String(encodedMessage, Charset.forName("UTF-8"));
-
-            return plainText;
-
-
-
-        }//try
-        catch (InvalidKeyException e) {
-            throw new IllegalArgumentException(
-                    "key argument does not contain a valid AES key");
-        } catch (BadPaddingException e) {
-            // you'd better know about padding oracle attacks
-            return null;
-        } catch (GeneralSecurityException e) {
-            throw new IllegalStateException(
-                    "Unexpected exception during decryption", e);
-        }
-    }
-
+		}// try
+		catch (InvalidKeyException e) {
+			throw new IllegalArgumentException(
+					"key argument does not contain a valid AES key");
+		} catch (BadPaddingException e) {
+			// you'd better know about padding oracle attacks
+			return null;
+		} catch (GeneralSecurityException e) {
+			throw new IllegalStateException(
+					"Unexpected exception during decryption", e);
+		}
+	}
 
 }
--- src/Server.java
+++ src/Server.java
@@ -5,11 +5,19 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.Security;
+import java.text.SimpleDateFormat;
 import java.util.ConcurrentModificationException;
-import java.util.HashSet;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
-/*Adapted from http://cs.lmu.edu/~ray/notes/javanetexamples/
+
+/**
+ * Adapted from http://cs.lmu.edu/~ray/notes/javanetexamples/
  * 
  * 
  */
@@ -22,10 +30,16 @@ public class Server {
 
 	static HashSet<String> names = new HashSet<String>();
 	static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
+	static Hashtable<String, ConnectionHandler> table = new Hashtable<String, ConnectionHandler>();
 	static ServerSocket server;
 	static ServerConsole console = new ServerConsole();
+	static Encryption enc = new Encryption();
+	static final String symKeyHex = "000102030405060708090A0B0C0D0E0F";
 
 	public static void main(String[] args) throws Exception {
+		
+		Security.addProvider(new BouncyCastleProvider());
+
 
 		server = new ServerSocket(port);
 
@@ -47,7 +61,8 @@ public class Server {
 
 		try {
 			for (PrintWriter writer : writers) {
-				writer.println(string);
+				//writer.println(string);
+				writer.println(enc.encrypt(string, symKeyHex));	//encryption version
 			}
 		} catch (ConcurrentModificationException e) {
 			System.err.println("Collection modified while iterating through");
@@ -58,11 +73,13 @@ public class Server {
 	// one handler for each client
 	private static class ConnectionHandler extends Thread {
 
-		private Date date = new Date();
+		private static SimpleDateFormat date = new SimpleDateFormat(
+				"yyyy.MM.dd HH:mm:ss.SS");
 		private String name;
 		private Socket socket;
 		private BufferedReader in;
 		private PrintWriter out;
+		private static Date now = new Date();
 
 		public ConnectionHandler(Socket socketIn) {
 			this.socket = socketIn;
@@ -70,7 +87,7 @@ public class Server {
 
 		public void run() {
 
-			System.out.println("New connection accepted");
+			System.out.println("New connection accepted " + date.format(now));
 
 			try {
 				in = new BufferedReader(new InputStreamReader(
@@ -82,8 +99,12 @@ public class Server {
 			}
 			try {
 				while (true) {
-					out.println("SUBMITNAME");
-					name = in.readLine();
+
+					out.println(enc.encrypt("SUBMITNAME", symKeyHex));
+					//out.println("SUBMITNAME");	//this is the old version for the line above
+					
+					//name = enc.decrypt(in.readLine(), symKeyHex);
+					name = in.readLine();		//this is the old version for the line above
 					if (name == null) {
 						return;
 					}
@@ -94,21 +115,45 @@ public class Server {
 						}
 					}
 				}
-				out.println("NAMEACCEPTED");
+				
+				out.println(enc.encrypt("NAMEACCEPTED", symKeyHex));
+				//out.println("NAMEACCEPTED");		//this is the old version for the line above
 				System.out.println("Added client: " + name);
 				writers.add(out);
+				table.put(name, this);
 
+				String input;
+				
 				while (true) {
-					String input = in.readLine();
+					
+					//input = enc.decrypt(in.readLine(), symKeyHex);	//encryption version here
+					input = in.readLine();	//old version here
+					
 					if (input == null) {
 						return;
 					}
-					sendMessage("MESSAGE " + name + ": " + input);
+
+					if (input == "<DISCONNECT>") {
+						break;
+					} else if (input == "<ME>") {
+						sendMessage("MESSAGE " + name + " has disconnected.");
+					} else {
+						sendMessage("MESSAGE " + name + ": " + input);
+
+					}
+
 				}
 
 			} catch (IOException e) {
-				System.err.println("Name Error");
-				e.printStackTrace();
+
+				if (name == null) {
+					System.err.println("Name Error");
+				} else if (e.getMessage() == "Connection reset") {
+					System.out.println("a client disconnected");
+				} else {
+					e.printStackTrace();
+
+				}
 			}
 
 			closeConnection();
@@ -128,19 +173,8 @@ public class Server {
 			}
 		}
 
-		/**
-		 * @return the date
-		 */
-		public Date getDate() {
-			return date;
-		}
-
-		/**
-		 * @param date
-		 *            the date to set
-		 */
-		public void setDate(Date date) {
-			this.date = date;
+		public static String getCreationTime() {
+			return date.format(now);
 		}
 
 	}
@@ -176,6 +210,9 @@ public class Server {
 					case "query":
 						queryUser(args);
 						break;
+					case "rawwrite":
+						rawWrite(args);
+						break;
 					}
 
 				}
@@ -184,6 +221,14 @@ public class Server {
 				System.err.println("Error trying to read input");
 				e.printStackTrace();
 			}
+		}
+
+		private void rawWrite(String args2) {
+
+			if (args2.startsWith("-")) {
+
+			}
+
 		}
 
 		private void processArgs() {
@@ -196,11 +241,18 @@ public class Server {
 				args = fromUser.substring(5).trim();
 				fromUser = "query";
 			}
+			if (fromUser.startsWith("rawwrite")) {
+				args = fromUser.substring(8).trim();
+				fromUser = "rawwrite";
+			}
+
 		}
 
+		// prints out info about the user (time only for now)
 		private void queryUser(String args2) {
-			// need some way of matching username in hashset with date
 
+			// prints out when the client was created
+			System.out.println(table.get(args2).getCreationTime());
 		}
 
 		private void listClients() {
